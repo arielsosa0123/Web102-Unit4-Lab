015f97a3f61575199421ff948521eeabb9ca170e
Ariel Sosa <arielsosa0123@gmail.com>
2026-06-24 21:59:49 -0400

    Completed Step 1

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import APIForm from './Components/APIForm';
+import './App.css'
+
+const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
+
+function App() {
+  const [inputs, setInputs] = useState({
+    url: "",
+    format: "",
+    no_ads: "",
+    no_cookie_banners: "",
+    width: "",
+    height: "",
+  });
+
+  const submitForm = () => {
+
+  };
+
+  return (
+    <div className="whole-page">
+      <h1>Build Your Own Screenshot! 📸</h1>
+      
+      <APIForm
+        inputs={inputs}
+        handleChange={(e) =>
+          setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value.trim(),
+          }))
+        }
+        onSubmit={submitForm}
+      />
+      <br></br>
+
+    </div>
+  );
+}
+
+export default App
